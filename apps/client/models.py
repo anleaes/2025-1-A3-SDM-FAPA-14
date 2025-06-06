673f830ff56a77d8cd84ead39f57b9b087fd30ad
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-06 20:08:37 -0300

    update/models-client

--- apps/client/models.py
+++ apps/client/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
-    name = models.CharField('Nome', max_length=50)
+    name = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail', null=False, blank=False)
+    cell_phone = models.CharField('Telefone celular', max_length=20)
+    GENDER_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+        ('O', 'Outro'),
+    ]
+    gender = models.CharField('Gênero', max_length=1, choices=GENDER_CHOICES)
 
     class Meta:
         verbose_name = 'Cliente'
