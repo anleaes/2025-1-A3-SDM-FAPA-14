a1f5bae9acd87b000e60d2e4293029faa39ffe49
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-06 19:58:07 -0300

    initial structure models

--- apps/client/models.py
+++ apps/client/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Client(models.Model):
+    name = models.CharField('Nome', max_length=50)
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
