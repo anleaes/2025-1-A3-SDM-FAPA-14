1e1a43817f4e1cb5f52ab36f032af0708aa4eb36
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-06 20:35:08 -0300

    update-views/client

--- apps/client/views.py
+++ apps/client/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from .serializer import ClientSerializer
 
 # Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
