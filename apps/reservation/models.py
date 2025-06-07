336da73cd59915c4ca5336bdbe3a01713acd947f
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-07 20:32:01 -0300

    create models-reservation

--- apps/reservation/models.py
+++ apps/reservation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    checkin_date = models.DateField()
+    checkout_date = models.DateField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField('Status')
+
+    class Meta:
+        verbose_name = 'Reserva'
+        verbose_name_plural = 'Reservas'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
