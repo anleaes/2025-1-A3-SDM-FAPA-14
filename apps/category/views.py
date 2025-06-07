17a96abde4f02c136b9235da0def688e1c06adaf
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-07 18:06:49 -0300

    update views-category

--- apps/category/views.py
+++ apps/category/views.py
@@ -1,3 +1,8 @@
-
+from .models import Category
+from rest_framework import viewsets
+from .serializer import CategorySerializer
 
 # Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
