31dec78d06cf35f13c8ac2a4b4ff5cf4d569f7cf
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-06 21:24:46 -0300

    update models/hosting

--- apps/hosting/models.py
+++ apps/hosting/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Hosting(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    description = models.TextField('Descrição', max_length=100)
+    daily_price = models.DecimalField('Preço diário', max_digits=10, decimal_places=2)
+    address = models.CharField('Endereco', max_length=200)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Hospedagem'
+        verbose_name_plural = 'Hospedagens'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
