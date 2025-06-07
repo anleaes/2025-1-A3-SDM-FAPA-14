50120780c12b0d99a047df7e0b4340b7e9980d67
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-06 21:33:41 -0300

    update views-hosting

--- apps/hosting/views.py
+++ apps/hosting/views.py
@@ -1,3 +1,8 @@
-
+from .models import Hosting
+from rest_framework import viewsets
+from .serializer import HostingSerializer
 
 # Create your views here.
+class HostingViewSet(viewsets.ModelViewSet):
+    queryset = Hosting.objects.all()
+    serializer_class = HostingSerializer
