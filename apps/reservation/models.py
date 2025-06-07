10d06107d21207ad404094f3548aa188dc1c8845
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-07 20:50:28 -0300

    feature foreingkey reservation-client

--- apps/reservation/models.py
+++ apps/reservation/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from client.models import Client
 
 # Create your models here.
 class Reservation(models.Model):
-    checkin_date = models.DateField()
-    checkout_date = models.DateField()
+    checkin_date = models.DateField('Data de Check-in')
+    checkout_date = models.DateField('Data de Check-out')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.BooleanField('Status')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Reserva'
