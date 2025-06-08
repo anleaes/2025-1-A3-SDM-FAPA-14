5d2d08d7efce0b90c4f1a87c627ddb3c45cf8d3b
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-08 19:21:08 -0300

    update models-reservationhosting

--- apps/reservationhosting/models.py
+++ apps/reservationhosting/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from reservation.models import Reservation
+from hosting.models import Hosting
 
 # Create your models here.
 class ReservationHosting(models.Model):
     quantity_days = models.IntegerField('Quantidade de dias')
     unit_price = models.DecimalField('Preço unitário', max_digits=10, decimal_places=2)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE, null=True, blank=True)
+    hosting = models.ForeignKey(Hosting, on_delete=models.CASCADE, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Reserva de Hospedagem'
