d3edf8cb38cc28c0a58a288af57b22724a642a16
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-08 19:37:51 -0300

    update views-reseravtionhosting and serializer

--- apps/reservationhosting/views.py
+++ apps/reservationhosting/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import ReservationHosting
+from rest_framework import viewsets
+from .serializer import ReservationHostingSerializer
 
 # Create your views here.
+class ReservationHostingViewSet(viewsets.ModelViewSet):
+    queryset = ReservationHosting.objects.all()
+    serializer_class = ReservationHostingSerializer
