bf7e7df6ce3ebd3ba32ba6bc458af60ac0cc3c7c
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-08 22:01:57 -0300

    create models-review

--- apps/review/models.py
+++ apps/review/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    rating = models.IntegerField('Avaliação', default=0)
+    comment = models.TextField('Comentário', blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Avaliação'
+        verbose_name_plural = 'Avaliações'
+
+    def __str__(self):
+        return self.name
