9005e6a32d90c3b2bcb828fd675480fea2533565
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-10 21:15:54 -0300

    start models-address

--- apps/address/models.py
+++ apps/address/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Address(models.Model):
+    street = models.CharField()
+    city = models.CharField()
+    state = models.CharField()
+    postal_code = models.CharField()
+
+    class Meta:
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
+
+    def __str__(self):
+        return self.name
