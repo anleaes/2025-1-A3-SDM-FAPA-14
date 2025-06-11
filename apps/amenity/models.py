5c4394840ab6aa2ab64f0ceb7b2671b7ea1e5e1e
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-11 19:45:11 -0300

    start models-amenity

--- apps/amenity/models.py
+++ apps/amenity/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Amenity(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    is_available = models.BooleanField(default=True)
+    extra_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    class Meta:
+        verbose_name = 'Comodidade'
+        verbose_name_plural = 'Comodidades'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
