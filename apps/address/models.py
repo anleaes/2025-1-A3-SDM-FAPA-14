6942ab671758ea6ac1149c95bbdcec84cdea746b
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-10 21:16:06 -0300

    update models-address

--- apps/address/models.py
+++ apps/address/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Address(models.Model):
-    street = models.CharField()
-    city = models.CharField()
-    state = models.CharField()
-    postal_code = models.CharField()
+    street = models.CharField('Rua', max_length=255)
+    city = models.CharField('Cidade', max_length=100)
+    state = models.CharField('Estado', max_length=100)
+    postal_code = models.CharField('CEP', max_length=20)
 
     class Meta:
         verbose_name = 'Endereço'
         verbose_name_plural = 'Endereços'
 
     def __str__(self):
-        return self.name
+        return f"{self.street}, {self.city} - {self.state} {self.postal_code}"
