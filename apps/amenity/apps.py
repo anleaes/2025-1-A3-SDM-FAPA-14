595daddedd48bd0e368db064d4638390d9143f68
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-11 19:39:01 -0300

    setup settings and app-amenity

--- apps/amenity/apps.py
+++ apps/amenity/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AmenityConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'amenity'
+    verbose_name = 'Comodidade'
--- hospedagemapp/settings.py
+++ hospedagemapp/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'reservationhosting.apps.ReservationHostingConfig',
     'review.apps.ReviewConfig',
     'address.apps.AddressConfig',
+    'amenity.apps.AmenityConfig',
 ]
 
 MIDDLEWARE = [
