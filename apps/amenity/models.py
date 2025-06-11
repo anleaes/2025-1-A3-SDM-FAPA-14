843821cc58a447c3983cab699acdb19fd668eb0c
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-11 19:48:40 -0300

    update models-amenity

--- apps/amenity/models.py
+++ apps/amenity/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Amenity(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(blank=True, null=True)
-    is_available = models.BooleanField(default=True)
-    extra_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    name = models.CharField('Nome', max_length=100, unique=True)
+    description = models.TextField('Descrição', max_length=100)
+    is_available = models.BooleanField('Disponível', default=True)
+    extra_cost = models.DecimalField('Custo Extra', max_digits=10, decimal_places=2, default=0.00)
 
     class Meta:
         verbose_name = 'Comodidade'
