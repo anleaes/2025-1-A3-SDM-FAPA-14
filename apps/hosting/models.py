f694f3f52cdf3166f5325755cc793dd053d3ade4
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-13 21:42:06 -0300

    refactor:adjust verbose_name for foreingkeys

--- apps/hosting/models.py
+++ apps/hosting/models.py
@@ -8,8 +8,8 @@ class Hosting(models.Model):
     description = models.TextField('Descrição', max_length=100)
     daily_price = models.DecimalField('Preço diário', max_digits=10, decimal_places=2)
     is_available = models.BooleanField('Disponível')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, verbose_name='Categoria', on_delete=models.CASCADE, null=True)
+    address = models.OneToOneField(Address, verbose_name='Endereço', on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = 'Hospedagem'
