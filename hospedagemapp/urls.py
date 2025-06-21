7dcc5aa3734646507e177dd535b3fe6eec03b2d7
Gabriel Ávila dos Santos <gabrielsnt.avila@gmail.com>
2025-06-21 16:51:34 -0300

    add: setup token to client

--- hospedagemapp/urls.py
+++ hospedagemapp/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +30,7 @@ urlpatterns = [
     path('reservas/', include('reservation.urls', namespace='reservation')),
     path('itens_reserva/', include('reservationhosting.urls', namespace='reservationhosting')),
     path('avaliacoes/', include('review.urls', namespace='review')),
+    path('token-autenticacao/', obtain_auth_token)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
